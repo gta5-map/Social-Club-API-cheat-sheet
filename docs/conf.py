951bd62d865f659efd8133ee47cb5368dace8b29
Jonas Friedmann <j@frd.mn>
2017-01-14 10:31:18 +0100

    Remove Markdown support

--- docs/conf.py
+++ docs/conf.py
@@ -34,12 +34,8 @@ extensions = []
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
-source_parsers = {
-    '.md': CommonMarkParser,
-}
-
 # The suffix of source filenames.
-source_suffix = ['.rst', '.md']
+source_suffix = ['.rst']
 
 # The encoding of source files.
 #source_encoding = 'utf-8-sig'
